9a89d331e62a41bfd3827068bb8ec1bbab7826ab
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-11-20 16:47:16 -0500

    update BiocProject examples

--- example_BiocProject/parseEncodeRegions.R
+++ example_BiocProject/parseEncodeRegions.R
@@ -1,15 +1,11 @@
 parseEncodeRegions = function(project) {
-  # get the data from the Project config
+  # get the data from the Project object
   url = pepr::samples(project)$remote_url[[1]]
-  sampleName = pepr::samples(project)$sample_name[[1]]
-  fileName = pepr::samples(project)$file_name[[1]]
-  workDir = pepr::config(project)$metadata$output_dir
-  # create the download dir if it does not exist
   # download the file
   bfc = BiocFileCache::BiocFileCache(cache = tempdir(),ask = FALSE)
   path = BiocFileCache::bfcrpath(x = bfc, url)
   # read it in
-  df=read.table(path)
+  df = read.table(path)
   # formatting
   colnames(df) = c('chr', 'start', 'end', 'name')
   # convert to GRanges object
--- example_BiocProject/parseEncodeRegions_resize.R
+++ example_BiocProject/parseEncodeRegions_resize.R
@@ -1,10 +1,6 @@
 parseEncodeRegions_resize = function(project, resize.width) {
   # get the data from the Project config
   url = pepr::samples(project)$remote_url[[1]]
-  sampleName = pepr::samples(project)$sample_name[[1]]
-  fileName = pepr::samples(project)$file_name[[1]]
-  workDir = pepr::config(project)$metadata$output_dir
-  # create the download dir if it does not exist
   # download the file
   bfc = BiocFileCache::BiocFileCache(cache = tempdir(),ask = FALSE)
   path = BiocFileCache::bfcrpath(x = bfc, url)
